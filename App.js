7779805e5b059fff64dec818da215a76a65f2edb
Rahav89 <132464873+Rahav89@users.noreply.github.com>
2024-01-29 21:42:52 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import FormGrade from './ClassComps/CCCForm';
+import TableComponent from './ClassComps/CCCTable';
+import ColorBth from './ClassComps/CCColorBth';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorBth />
+      <FormGrade />
+      <TableComponent />
+    </div>
+  );
+}
+
+export default App;
